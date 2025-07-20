0e76d1899617ec289e321a9bb42779bb564eb46b
Neil Ivan V. Tanamor <neilvanromanat@gmail.com>
2025-07-20 12:31:11 +0800

    Fix for GitHub Pages deployment (base href + .nojekyll)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1303169841"
+    serviceWorkerVersion: "2497539071"
   }
 });
